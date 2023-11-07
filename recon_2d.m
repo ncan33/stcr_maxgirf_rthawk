91a1fe006e83b0f418fbe7c341310f5bfb5b5d8e
Nejat Can <ncan@u.rochester.edu>
2023-11-07 08:25:27 -0800

    small change

--- recon_2d.m
+++ recon_2d.m
@@ -7,7 +7,7 @@ function image_coil_combined = recon_2d(nr_arms_per_frame, TR_to_trim, ...
     arguments
         nr_arms_per_frame = 61
         TR_to_trim = 15*61
-        maxgirf_flag = 1
+        maxgirf_flag = 0
         area = 'pulseq_lung'
         which_file = 1
         useGPU = 1
